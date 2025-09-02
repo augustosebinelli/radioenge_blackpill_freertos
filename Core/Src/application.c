71eaad79850417e220e416e8eacb87017cbf3196
augustosebinelli <augustosebinelli@alunos.utfpr.edu.br>
2025-09-02 14:16:13 -0300

    AULA4

--- Core/Src/application.c
+++ Core/Src/application.c
@@ -1,6 +1,8 @@
 #include "cmsis_os.h"
 #include "stm32f4xx_hal.h"
 #include "radioenge_modem.h"
+#include "main.h"
+
 //testando
 extern osTimerId_t PeriodicSendTimerHandle;
 extern osThreadId_t AppSendTaskHandle;
@@ -32,3 +34,37 @@ void AppSendTaskCode(void *argument)
         osDelay(30000);
     }
 }
+
+void BlueLedTaskCode(void *argument){
+
+    while(1){
+        HAL_GPIO_TogglePin(LED4_BLUE_GPIO_Port,LED4_BLUE_Pin) ;
+        osDelay (50) ; // Espera 50 ms)
+    }
+}
+
+void GreenLedTaskCode(void *argument){
+
+    while(1){
+        HAL_GPIO_TogglePin(LED3_GREEN_GPIO_Port,LED3_GREEN_Pin) ;
+        osDelay (50) ; // Espera 50 ms)
+    }
+}
+
+void YellowLedTaskCode(void *argument){
+
+    while(1){
+        HAL_GPIO_TogglePin(LED2_YELLOW_GPIO_Port,LED2_YELLOW_Pin) ;
+        osDelay (50) ; // Espera 50 ms)
+    }
+}
+
+void RedLedTaskCode(void *argument){
+
+    while(1){
+        HAL_GPIO_TogglePin(LED1_RED_GPIO_Port,LED1_RED_Pin) ;
+        osDelay (50) ; // Espera 50 ms)
+    }
+}
+
+
